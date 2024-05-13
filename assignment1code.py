8d6b8eb6c0782d1d02a8831e88630ded0d3c53d2
OreLana <lana.oreoluwa2@gmail.com>
2024-05-13 08:01:16 -0600

    added name to assignment and readme

---
+++ assignment1code.py
@@ -0,0 +1,12 @@
+# ENSF 692 Spring 2024
+# Assignment 1 Starter Code
+# Add your name here!
+# Oreoluwa Lana
+
+studentName = "Oreoluwa Lana"  # Change the contents of this variable to your name (leave the quotation marks).
+
+print("Hello, World!")
+print("My name is " + studentName + " and I am a student in ENSF 692.")
+
+# print your best skill here
+print("I am good at Cooking")
